799e594b529bceb5f2b82b1af0e6f4eabd4e6b77
Gokce Gokmen <51439620+A01258386@users.noreply.github.com>
2021-12-18 00:44:15 -0800

    project completed

--- index.js
+++ index.js
@@ -1,70 +1,60 @@
-const { request } = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const session = require("express-session");
-const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 8007;
 const app = express();
-const fs = require("fs");
-
+const { v4: uuid } = require("uuid");
+const fs = require("fs/promises");
 // Don't worry about these 4 lines below
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(expressLayouts);
 
-app.use((req, res, next) => {
-  res.locals = ({
-    user: req.user || null,
-  });
-  next()
+
+app.get("/", (req, res) => {
+  res.render("createcard");
 });
 
-app.use(express.static(__dirname + '/public'));
+app.post("/", async (req, res) => {
+  const data = req.body;
+  data.id = uuid().substring(0, 5);
+  //react database.json
 
-app.get("/people/:id", (req, res) => {
-  res.render("people");
+  //filter that values equeal to 'on'
+  let knownTechnologies = Object.entries(data).filter(([key, value]) => {
+    if (value === "on") {
+      delete data[key];
+      return true;
+    }
+  }).map(item => item[0]);
+  data.knownTechnologies = knownTechnologies;
+  data.favoriteBooks = data.books.split(",");
+  delete data.books;
+  data.favoriteArtists = data.artists.split(",");
+  delete data.artists;
+
+  try {
+    let db = JSON.parse(await fs.readFile("database.json", "utf8")).users;
+    console.log(db);
+    db.push(data);
+    await fs.writeFile("database.json", JSON.stringify({ users: db }));
+    res.redirect("/people/"+data.id);
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+app.get("/people/:id", async (req, res) => {
+  let id = req.params.id;
+  let db = JSON.parse(await fs.readFile("database.json", "utf8")).users;
+  let person = db.find(person => person.id === id);
+  res.render("people", { person });
 });
 
-app.get("/homepage", (req, res) => {
-  res.render("homepage");
-});
 
-
-//write the id to the database.json file users
-
-//read database.json file
-
-app.get("/:id", (req, res) => {
+app.get("/:id/photos", (req, res) => {
   const id = req.params.id;
-  const data = fs.readFileSync("./database.json");
-  const jsonData = JSON.parse(data);
-  const user = jsonData.users.find(user => user.id === id);
-  res.render("profile", { user });
 });
-
-//write to json file
-app.post("/:id", (req, res) => {
-  const id = req.params.id;
-  const data = fs.readFileSync("./database.json");
-  const jsonData = JSON.parse(data);
-  const user = jsonData.users.find(user => user.id === id);
-  user.fullName = req.body.fullName;
-  user.aboutMe = req.body.aboutMe;
-  user.knownTechnologies = req.body.knownTechnologies;
-  fs.writeFileSync("./database.json", JSON.stringify(jsonData));
-  res.redirect(`/${id}`);
-});
-
-
 
 app.listen(PORT, () => {
   console.log(`Server now is running at http://localhost:${PORT} 🚀`);
 });
-
-
-
-// ejs
-// request.params
-// fs.writefile
